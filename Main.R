6f0da96148eb73c5ef7062de6d4a3fa10b19d1e3
Rainwright <nicholaschen191100@gmail.com>
2020-11-03 22:18:34 +0800

    Comment Commit

--- Main.R
+++ Main.R
@@ -1,9 +1,27 @@
 # install.packages('ggplot2')
+# install.packages('dplyr')
+# install.packages('shiny')
 library("ggplot2")
+library("dplyr")
+library("shiny")
 
 # import file, path may vary from pc to pc
 data = read.csv(file="D:/Downloads (HDD)/APU Courses/Degree Year 2/Sem 1/PFDA/Assignment/Program/weatherdata.csv", header=TRUE, sep=",")
 options(max.print=1000000)
 print(colnames(data))
+View(data)
 
-
+# set up server and ui
+# ui <- fluidPage(
+#   h1("My Shiny App"),
+#   p(style = "font-family:Impact",
+#     "See other apps in the",
+#     a("Shiny Showcase",
+#       href = "http://www.rstudio.com/
+#       products/shiny/shiny-user-showcase/")
+#   )
+# )
+# 
+# server <- function(input, output){}
+# 
+# shinyApp(ui = ui, server = server)
